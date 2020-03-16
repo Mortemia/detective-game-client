28923361edee4db54f3473c37019485bcde44f9b
Aldona Pieniążek <aldona97@gmail.com>
2020-03-16 12:35:28 +0100

    File pl.json updated, init i18n-react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import T from 'i18n-react';
 import AppRouter from './routers/AppRouter';
 import { ThemeProvider } from '@material-ui/core/styles';
 import { BrowserRouter } from 'react-router-dom';
 import theme from './theme';
 import GameProvider from './context/gameContext';
 import AppProvider from './context/appContext';
+
+const dictionary = require('./localization/translations/pl.json');
+T.setTexts(dictionary);
 
 function App() {
   return (
--- src/pages/Home/InfoGrid.js
+++ src/pages/Home/InfoGrid.js
@@ -1,37 +1,38 @@
-import React from "react";
+import React from 'react';
 import FingerprintIcon from '@material-ui/icons/Fingerprint';
-import MapIcon from "@material-ui/icons/Map";
+import MapIcon from '@material-ui/icons/Map';
 import ForumIcon from '@material-ui/icons/Forum';
 import ExtensionIcon from '@material-ui/icons/Extension';
-import Grid from "@material-ui/core/Grid";
-import InfoTile from "./InfoTile";
-import { makeStyles } from "@material-ui/core/styles";
+import Grid from '@material-ui/core/Grid';
+import InfoTile from './InfoTile';
+import { makeStyles } from '@material-ui/core/styles';
+import T from 'i18n-react';
 
-const infos = [
+const infos = () => [
   {
-    header: "Badaj",
-    icon: FingerprintIcon
+    header: T.translate('examine'),
+    icon: FingerprintIcon,
   },
   {
-    header: "Przesłuchuj",
-    icon: ForumIcon
+    header: T.translate('travel'),
+    icon: ForumIcon,
   },
   {
-    header: "Podróżuj",
-    icon: MapIcon
+    header: 'Podróżuj',
+    icon: MapIcon,
   },
   {
-    header: "Łącz fakty",
-    icon: ExtensionIcon
-  }
+    header: 'Łącz fakty',
+    icon: ExtensionIcon,
+  },
 ];
 
 const useStyles = makeStyles(theme => ({
   root: {
     marginLeft: theme.spacing(6),
     marginRight: theme.spacing(6),
-    marginTop: theme.spacing(6)
-  }
+    marginTop: theme.spacing(6),
+  },
 }));
 
 const InfoGrid = () => {
@@ -40,9 +41,9 @@ const InfoGrid = () => {
   return (
     <div className={classes.root}>
       <Grid container spacing={6}>
-        {infos.map((info, index) => (
+        {infos().map((info, index) => (
           <Grid item xs={12} sm={12} md={3} key={index}>
-            <InfoTile icon={info.icon} header={info.header}/>
+            <InfoTile icon={info.icon} header={info.header} />
           </Grid>
         ))}
       </Grid>
