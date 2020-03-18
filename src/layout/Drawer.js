1c7f0b6249489bd372fc3d36a919bb5f6c44af42
Aldona Pieniążek <aldona97@gmail.com>
2020-03-18 11:46:44 +0100

    GameplayBar in a seperate file with game infos

--- src/layout/Drawer.js
+++ src/layout/Drawer.js
@@ -3,17 +3,12 @@ import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
-import Badge from '@material-ui/core/Badge';
-import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import NotificationsIcon from '@material-ui/icons/Notifications';
 import { mainListItems, secondaryListItems } from './listItems';
-import Logo from './Logo';
+import { GameContext } from '../context/gameContext';
 
 const drawerWidth = 240;
 
@@ -97,70 +92,36 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-const ComplexDrawer = ({ children }) => {
+const ComplexDrawer = ({ open, handleDrawerClose, children }) => {
   const classes = useStyles();
-  //const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
-
-  const [open, setOpen] = React.useState(false);
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
+  const { game, dispatch } = React.useContext(GameContext);
 
   return (
-    <>
-      <div className={classes.root}>
-        <CssBaseline />
-        <AppBar
-          position='absolute'
-          className={clsx(classes.appBar, open && classes.appBarShift)}
-        >
-          <Toolbar className={classes.toolbar}>
-            <IconButton
-              edge='start'
-              color='inherit'
-              aria-label='open drawer'
-              onClick={handleDrawerOpen}
-              className={clsx(
-                classes.menuButton,
-                open && classes.menuButtonHidden
-              )}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Logo />
-            <IconButton color='inherit'>
-              <Badge badgeContent={4} color='secondary'>
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
-          </Toolbar>
-        </AppBar>
-        <Drawer
-          variant='permanent'
-          classes={{
-            paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
-          }}
-          open={open}
-        >
-          <div className={classes.toolbarIcon}>
-            <IconButton onClick={handleDrawerClose}>
-              <ChevronLeftIcon />
-            </IconButton>
-          </div>
-          <Divider />
-          <List>{mainListItems}</List>
-          <Divider />
-          <List>{secondaryListItems}</List>
-        </Drawer>
-        <main className={classes.content}>
-          <div className={classes.appBarSpacer} />
-          {children}
-        </main>
-      </div>
-    </>
+    <div className={classes.root}>
+      <CssBaseline />
+
+      <Drawer
+        variant='permanent'
+        classes={{
+          paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+        }}
+        open={open}
+      >
+        <div className={classes.toolbarIcon}>
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+        <Divider />
+        <List>{mainListItems}</List>
+        <Divider />
+        <List>{secondaryListItems}</List>
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.appBarSpacer} />
+        {children}
+      </main>
+    </div>
   );
 };
 
--- src/layout/Gameplay.js
+++ src/layout/Gameplay.js
@@ -3,13 +3,22 @@ import GameplayRouter from '../routers/GameplayRouter';
 import CustomizedSnackbars from '../components/Snackbar';
 import { AppContext } from '../context/appContext';
 import Drawer from './Drawer';
+import GameplayBar from './GameplayBar';
 
 const Gameplay = () => {
   const { appState, appDispatch } = useContext(AppContext);
   const snackbar = appState.snackbar;
+  const [open, setOpen] = React.useState(false);
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
   return (
-    <Drawer>
+    <Drawer open={open} handleDrawerClose={handleDrawerClose}>
+      <GameplayBar open={open} handleDrawerOpen={handleDrawerOpen} />
       <GameplayRouter />
       <CustomizedSnackbars
         open={snackbar && snackbar.visible}
