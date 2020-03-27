4754502cd7deadc9500db3fa49b4f9e0f4c9d386
Aldona Pieniążek <aldona97@gmail.com>
2020-03-27 19:04:48 +0100

    Updates

---
+++ precache-manifest.21c69fdcf46712e983912d3d15c8ec4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67580ee4bceacd7f54d5a642b1efbd50",
+    "url": "/detective-game-client/index.html"
+  },
+  {
+    "revision": "46e44ac826806036ce20",
+    "url": "/detective-game-client/static/css/2.14bb1e62.chunk.css"
+  },
+  {
+    "revision": "9606b0629ff3420e0c07",
+    "url": "/detective-game-client/static/css/main.ff3e0b43.chunk.css"
+  },
+  {
+    "revision": "46e44ac826806036ce20",
+    "url": "/detective-game-client/static/js/2.75dfa72e.chunk.js"
+  },
+  {
+    "revision": "b9bc2fc7725df2ce4b7409c91212bc51",
+    "url": "/detective-game-client/static/js/2.75dfa72e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9606b0629ff3420e0c07",
+    "url": "/detective-game-client/static/js/main.5e8c932e.chunk.js"
+  },
+  {
+    "revision": "f591b4187c536d2a0069",
+    "url": "/detective-game-client/static/js/runtime-main.d2122551.js"
+  }
+]);
