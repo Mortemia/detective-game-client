27dd68785745cd811ddec8efcf99450ea7dad638
Aldona Pieniążek <aldona97@gmail.com>
2020-04-26 17:48:39 +0200

    Redirection to login page if not logged in

--- src/routers/GameplayRouter.js
+++ src/routers/GameplayRouter.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import Dashboard from '../pages/_gameplay/Dashboard';
 import People from '../pages/_gameplay/People';
 import Items from '../pages/_gameplay/Items';
 import Actions from '../pages/_gameplay/Actions';
 import Locations from '../pages/_gameplay/Locations';
+import { AppContext } from '../context/appContext';
+
 const GameplayRouter = () => {
+  const { appState } = React.useContext(AppContext);
+
   return (
-    <Switch>
-      <Route exact path='/play' component={Dashboard} />
+    <>
+      {!appState.user && <Redirect to='/login' />}
+      <Switch>
+        <Route exact path='/play' component={Dashboard} />
 
-      <Route exact path='/play/items/:id' component={Items} />
-      <Route path='/play/items' component={Items} />
+        <Route exact path='/play/items/:id' component={Items} />
+        <Route path='/play/items' component={Items} />
 
-      <Route exact path='/play/people/:id' component={People} />
-      <Route path='/play/people' component={People} />
+        <Route exact path='/play/people/:id' component={People} />
+        <Route path='/play/people' component={People} />
 
-      <Route exact path='/play/actions/:id' component={Actions} />
-      <Route path='/play/actions' component={Actions} />
+        <Route exact path='/play/actions/:id' component={Actions} />
+        <Route path='/play/actions' component={Actions} />
 
-      <Route path='/play/locations' component={Locations} />
-    </Switch>
+        <Route path='/play/locations' component={Locations} />
+      </Switch>
+    </>
   );
 };
 
