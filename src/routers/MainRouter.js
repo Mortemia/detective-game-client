1564fb61d15cb33a842970a6d49ba2e728fb7d44
Aldona Pieniążek <aldona97@gmail.com>
2020-04-29 22:37:39 +0200

    Redirect to login

--- src/routers/MainRouter.js
+++ src/routers/MainRouter.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import Home from '../pages/Home';
-import { Route, Switch } from 'react-router-dom';
-
+import { Route, Switch, Redirect } from 'react-router-dom';
 import HowToPlay from '../pages/HowToPlay';
 import Dashboard from '../pages/Dashboard';
 import LoginForm from '../pages/Login';
+import { AppContext } from '../context/appContext';
 
 const HomeRouter = () => {
+  const { appState } = React.useContext(AppContext);
+
   return (
     <>
       <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='/howtoplay' component={HowToPlay} />
-        <Route exact path='/dashboard' component={Dashboard} />
         <Route exact path='/login' component={LoginForm} />
+        {!appState.user ? (
+          <Redirect to='/login' />
+        ) : (
+          <Route exact path='/dashboard' component={Dashboard} />
+        )}
       </Switch>
     </>
   );
