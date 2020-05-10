8a350da4bc3fa5acfe800706ab7ae25f5868fe7c
Aldona Pieniążek <aldona97@gmail.com>
2020-05-10 14:57:27 +0200

    More TreeDiagram style changes

--- src/pages/_creator/Dashboard/TreeDiagram/index.js
+++ src/pages/_creator/Dashboard/TreeDiagram/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import OrgChart from '@dabeng/react-orgchart';
 import LinearProgress from '@material-ui/core/LinearProgress';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
 import Node from './Node';
 
+const useStyles = makeStyles(theme => ({
+  paper: {
+    padding: theme.spacing(2),
+    overflow: 'auto',
+    height: '100%',
+    overflow: 'hidden',
+  },
+}));
+
 const TreeDiagram = ({ data }) => {
+  const classes = useStyles();
+
   const [tree, setTree] = React.useState();
   const [warning, setWarning] = React.useState(null);
 
@@ -59,18 +73,25 @@ const TreeDiagram = ({ data }) => {
     }
   }, [data]);
 
-  return tree ? (
-    <OrgChart
-      datasource={tree}
-      pan={true}
-      zoom={true}
-      chartClass='myChart'
-      NodeTemplate={Node}
-    />
-  ) : warning ? (
-    warning
-  ) : (
-    <LinearProgress />
+  return (
+    <Paper className={classes.paper}>
+      <Typography component='h2' variant='h6' color='primary' gutterBottom>
+        Diagram komponentów
+      </Typography>
+      {tree ? (
+        <OrgChart
+          datasource={tree}
+          pan={true}
+          zoom={true}
+          chartClass='myChart'
+          NodeTemplate={Node}
+        />
+      ) : warning ? (
+        warning
+      ) : (
+        <LinearProgress />
+      )}
+    </Paper>
   );
 };
 
