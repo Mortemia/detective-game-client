796dd042fa9224099175967fbcfaf608d48f1998
Aldona Pieniążek <aldona97@gmail.com>
2020-05-11 23:34:18 +0200

    Routes code cleanup

--- src/routers/AuthorizedRoute.js
+++ src/routers/AuthorizedRoute.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import Cookie from 'js-cookie';
-import { Route, Switch, Redirect } from 'react-router-dom';
-import Gameplay from '../layout/Gameplay';
-import Creator from '../layout/Creator';
-import Main from '../layout/Main';
+import { Route, Redirect } from 'react-router-dom';
 
 const AuthorizedRoute = props => {
   const token = Cookie.get('token');
--- src/routers/GameplayRouter.js
+++ src/routers/GameplayRouter.js
@@ -8,36 +8,31 @@ import Locations from '../pages/_gameplay/Locations';
 import FinalTest from '../pages/_gameplay/FinalTest';
 import ResultDialog from '../pages/_gameplay/ResultDialog';
 import IntroductionDialog from '../pages/_gameplay/IntroductionDialog';
-import { AppContext } from '../context/appContext';
 import Route from './AuthorizedRoute';
 
 const GameplayRouter = () => {
-  const { appState } = React.useContext(AppContext);
-
   return (
-    <>
-      <Switch>
-        <Route exact path='/play' component={Dashboard} />
+    <Switch>
+      <Route exact path='/play' component={Dashboard} />
 
-        <Route exact path='/play/result' component={ResultDialog} />
-        <Route exact path='/play/intro' component={IntroductionDialog} />
+      <Route exact path='/play/result' component={ResultDialog} />
+      <Route exact path='/play/intro' component={IntroductionDialog} />
 
-        <Route exact path='/play/items/:id' component={Items} />
-        <Route path='/play/items' component={Items} />
+      <Route exact path='/play/items/:id' component={Items} />
+      <Route path='/play/items' component={Items} />
 
-        <Route exact path='/play/people/:id' component={People} />
-        <Route path='/play/people' component={People} />
+      <Route exact path='/play/people/:id' component={People} />
+      <Route path='/play/people' component={People} />
 
-        <Route exact path='/play/actions/:id' component={Actions} />
-        <Route path='/play/actions' component={Actions} />
+      <Route exact path='/play/actions/:id' component={Actions} />
+      <Route path='/play/actions' component={Actions} />
 
-        <Route path='/play/locations/:id' component={Locations} />
-        <Route path='/play/locations' component={Locations} />
+      <Route path='/play/locations/:id' component={Locations} />
+      <Route path='/play/locations' component={Locations} />
 
-        <Route path='/play/finalTest' component={FinalTest} />
-        <Route exact path='/play/:parameter' component={Dashboard} />
-      </Switch>
-    </>
+      <Route path='/play/finalTest' component={FinalTest} />
+      <Route exact path='/play/:parameter' component={Dashboard} />
+    </Switch>
   );
 };
 
